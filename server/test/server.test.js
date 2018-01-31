cfc8ec70d3fdade12e015812fa74ed03e7bca600
mobit <mobit@gmail.com>
2018-01-31 17:57:11 +0530

    get todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 var {App} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'first text'
+},{
+    text: 'second text'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then((err,res) => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +33,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -44,9 +52,23 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((er) => done(er));
             });
         });
+});
+
+describe('GET /todos',(done) => {
+    it('should all get todos', (done) => {
+        request(App)
+        .get('/todos')
+        .send({})
+        .expect(200)
+        .expect((res) =>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+
 });
